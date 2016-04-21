e736886496aff59981e56af48a500989390d97d7
Vikram Gosain <vikramgosain2@gmail.com>
2016-04-21 17:01:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vikram/footballreferee/red_card.java
@@ -0,0 +1,23 @@
+package com.example.vikram.footballreferee;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class red_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red_card);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
